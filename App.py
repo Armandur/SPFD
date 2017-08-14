247e3d7c88bca900360fa86b0420ebd1785cae62
Rasmus Pettersson Vik <rasmus.pettersson.vik@gmail.com>
2017-08-14 14:12:59 +0200

    Added support for the command line options

--- App.py
+++ App.py
@@ -1,27 +1,24 @@
 import DownloadHandler
+import argparse
 from Image import Image
 
-# parser = argparse.ArgumentParser(description="Download files from a sequentially repeating URL")
-# parser.add_argument("-b", "--begin", help="Start of URL", required=True)
-# parser.add_argument("-e", "--end", help="End of URL", required=True)
-# parser.add_argument("-f", "--first", help="Start value for sequence", default=0, type=int)
-# parser.add_argument("-l", "--last", help="End value for sequence", required=True, type=int)
-# parser.add_argument("-p", "--pad", help="Number of padding 0's", default=1, type=int)
-# parser.add_argument("-r", "--pre", help="String to prefix filename with", default="", type=str)
-# parser.add_argument("-x", "--ext", help="Extension, including dot (.)", default=".jpg", type=str)
-# args = vars(parser.parse_args())
-#
-# print(args)
+parser = argparse.ArgumentParser(description="Download files from a sequentially repeating URL")
+parser.add_argument("-b", "--begin", help="Start of URL", required=True)
+parser.add_argument("-e", "--end", help="End of URL", default="")
+parser.add_argument("-f", "--first", help="Start value for sequence", default=0, type=int)
+parser.add_argument("-l", "--last", help="End value for sequence", required=True, type=int)
+parser.add_argument("-p", "--pad", help="Number of padding 0's", default=1, type=int)
+parser.add_argument("-r", "--pre", help="String to prefix filename with", default="", type=str)
+parser.add_argument("-x", "--ext", help="Extension, including dot (.)", default=".jpg", type=str)
+args = vars(parser.parse_args())
 
-
-prefix = "http://sssscomic.com/comicpages/"
-end = ".jpg"
+print(args)
 
 _images = []
 
-for i in range(764, 769):
-    _images.append(Image(prefix, i, end))
+for i in range(args["first"], args["last"]+1):
+    _images.append(Image(args["begin"], i, args["end"]))
 
 download_h = DownloadHandler.DownloadHandler()
 
-download_h.downloadAll(_images, "Stand Still - Stay Silent - ", 3, ".jpg")
+download_h.downloadAll(_images, args["pre"], 3, args["ext"])
